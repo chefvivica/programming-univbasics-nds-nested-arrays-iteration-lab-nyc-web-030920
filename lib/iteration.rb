82a21f28a697721d96165e831b971f1c57561ed2
Vivica Ma <vivicacongma@gmail.com>
2020-02-17 15:40:07 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -49,7 +49,7 @@ def total_even_pairs(src)
     while i < src.length do 
       inner = src[i]
       if inner[0].to_i % 2 == 0 && inner[1].to_i  % 2 == 0 
-          total = (inner[0].to_i + inner[1].to_i)
+          total = (inner[0] + inner[1])
           total += 1 
       end 
       i += 1
